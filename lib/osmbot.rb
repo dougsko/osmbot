b132097c5f02cb3d1062343dc594a40c40b7dd93
Doug P <dougtko@gmail.com>
2018-10-26 00:04:48 -0400

    ...

--- lib/osmbot.rb
+++ lib/osmbot.rb
@@ -1,5 +1,41 @@
+require 'cinch'
 require "osmbot/version"
+#require 'osmbot/youtube'
 
 module Osmbot
-  # Your code goes here...
+    class Youtube
+        include Cinch::Plugin
+
+        set :prefix, //
+        match /(https:\/\/www\.youtube\.com\/watch\?v=[^\s]+)/, group: :url, method: :get_title
+
+        def execute(m, arg)
+            m.reply arg
+        end
+
+        def get_title(m, arg)
+            arg.match(/(https:\/\/www\.youtube\.com\/watch\?v=[^\s;:]+)/)
+            title = `/usr/local/bin/youtube-dl --get-title --skip-download \"#{$1}\"`
+            if title.size > 0
+                m.reply("[YouTube] " + title)
+            end
+        end
+    end
+
+    class Bot
+        def initialize
+            @bot = Cinch::Bot.new do
+                configure do |c|
+                    c.server = "irc.freenode.org"
+                    c.nick = "osmbot"
+                    c.channels = ["#dougskofoo"]
+                    c.plugins.plugins = [Youtube]
+                end
+            end
+        end
+
+        def start
+            @bot.start
+        end
+    end
 end
