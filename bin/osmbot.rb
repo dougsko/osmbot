62cf45bb3de0ae4ad3120bd5de7b3eba2f4577ff
Doug P <dougtko@gmail.com>
2018-10-26 11:38:02 -0400

    ...

--- bin/osmbot.rb
+++ bin/osmbot.rb
@@ -3,5 +3,5 @@
 require "bundler/setup"
 require "osmbot"
 
-bot = Osmbot::Bot.new("irc.freenode.net", "icculus", ["#opensourcemusicians"])
+bot = Osmbot::Bot.new("irc.freenode.net", "nomind", ARGV[0], ["#opensourcemusicians"])
 bot.start
--- lib/osmbot.rb
+++ lib/osmbot.rb
@@ -4,13 +4,14 @@ require 'osmbot/youtube'
 
 module Osmbot
     class Bot
-        def initialize(server, nick, channels)
+        def initialize(server, nick, password, channels)
             @bot = Cinch::Bot.new do
                 configure do |c|
                     c.server = server
                     c.nick = nick
                     c.realname = nick
                     c.user = nick
+                    c.password = password
                     c.channels = channels
                     c.plugins.prefix = //
                     c.plugins.plugins = [Youtube]
